3376035bb90dca7521897b8f3cf69b87f3b84c56
Josiah Anderson <riddlerforever@live.com>
2021-10-21 12:22:35 -0500

    added functionality for customers to create job postings, also added a custom color for buttons

--- app/controllers/job_controller.rb
+++ app/controllers/job_controller.rb
@@ -8,6 +8,7 @@ class JobController < ApplicationController
   def new
     @customer = Customer.find(current_customer.id)
     @job = Job.new
+    $SPECIALTY_TYPES.prepend("select")
     render :new
   end
 
@@ -21,5 +22,6 @@ class JobController < ApplicationController
       flash.now[:error] = "Job failed to post"
       render :new
     end
+    $SPECIALTY_TYPES.delete_at(0)
   end
 end
