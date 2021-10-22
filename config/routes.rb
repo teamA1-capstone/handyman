e2c76f27750349014756157da49feb7d69ecb6b2
Josiah Anderson <riddlerforever@live.com>
2021-10-21 20:17:27 -0500

    fixed a bug with show worker route introduced in last commit

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   devise_for :customers
   
   root to: redirect('/home')
-  
+
   get 'home', to: 'customer#home', as: 'home'
   get 'home/customer_profile', to: 'customer#customer_profile', as: 'customer_profile'
   
@@ -22,7 +22,8 @@ Rails.application.routes.draw do
 
   get 'home/worker', to: 'worker#home', as: 'worker_home'
   get 'home/worker/worker_profile', to: 'worker#worker_profile', as: 'worker_profile'
+  
   get 'home/worker_directory/:specialty_index', to: 'worker#index', as: 'worker_directory'
-  get 'home/worker_directory/:id', to: 'worker#show', as: 'worker'
+  get 'home/worker_directory/:specialty_index/:id', to: 'worker#show', as: 'worker'
 
 end
