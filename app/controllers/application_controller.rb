3758e6255e52d161ce35fc1b0f381c595a5b2c42
Souad Hadidi <hadidisouad@gmail.com>
2021-10-23 21:09:04 -0500

    Updated the application_controller.rb file to include the after_sign_in_path_for and the after_sign_out_path_for methods, These two methods help solve the issue of redirection to the worker page (instead of customer home page) after clicking submit.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,5 +11,21 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :street_address, :city, :state, :zip_code, :specialty])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :street_address, :city, :state, :zip_code, :specialty])
   end
+  # This method is used to override the automatice redirection of the submit 
+  # buttom (after signing-up) to be the worker's home page not the customer's.
+  def after_sign_in_path_for(resource)
+    if resource.is_a?(Customer)
+      home_path
+    else
+      worker_home_path
+    end
+  end
   
+  def after_sign_out_path_for(resource)
+    if $SWITCH == 1
+      home_path
+    else
+      worker_home_path
+    end
+  end
 end
