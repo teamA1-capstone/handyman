f389705312aa0e1a36471bcbce333444e056c3b9
Ashlee Miller <miller26@memphis.edu>
2021-11-04 16:52:08 -0500

    Added invisibility attribute to worker devise model class

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_04_201744) do
+ActiveRecord::Schema.define(version: 2021_11_04_213609) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2021_11_04_201744) do
     t.string "state"
     t.string "zip_code"
     t.string "specialty"
+    t.integer "invisibility"
     t.index ["email"], name: "index_workers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_workers_on_reset_password_token", unique: true
   end
