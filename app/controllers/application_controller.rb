76cfbb6cd2fa1b5915a0027d49b34f55d514bdd4
Souad Hadidi <souad@souads-air.uom.memphis.edu>
2021-11-18 16:01:59 -0600

    The worker can decide to show or hide themselves from the available worker page. They do so by going to the edit profile and toggle their show or hide status

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :street_address, :city, :state, :zip_code, :specialty])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :street_address, :city, :state, :zip_code, :specialty])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :street_address, :city, :state, :zip_code, :specialty, :invisibility])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :street_address, :city, :state, :zip_code, :specialty, :invisibility])
   end
   # This method is used to override the automatice redirection of the submit 
   # buttom (after signing-up) to be the worker's home page not the customer's.
